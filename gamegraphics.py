6a92f5c361b119002684aa4285a9407d0bcb7e2a
AliciaRus <116314121+AliciaRus@users.noreply.github.com>
2023-02-07 22:51:39 +0000

    ice cream graphic

--- gamegraphics.py
+++ gamegraphics.py
@@ -1,10 +1,19 @@
+#https://mcsp.wartburg.edu/zelle/python/graphics/graphics.pdf
 from graphics import *
-def main():
-  win = GraphWin("My Circle",100,100)
-  c=Circle(Point(50,50),10)
+def drawIceCream():
+  win = GraphWin("My Circle",400,400)
+  c=Circle(Point(50,30),10)
+  c.setFill("white")
   c.draw(win)
+  c=Circle(Point(50,50),10)
+  c.setFill("pink")
+  c.draw(win)
+  cone=Polygon(Point(40,60),Point(60,60),Point(50,100))
+  cone.setFill("brown")
+  cone.draw(win)
   win.getMouse()
   win.close()
 
-main()
+drawIceCream()
+
   
